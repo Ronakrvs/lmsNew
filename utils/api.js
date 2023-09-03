b1a67affbb311d52f345d8880b84c94310afe916
ronak <ronak@hoicko.net>
2023-09-03 18:14:56 +0530

    url change

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: `http://localhost:4001/api`, //YOUR_API_URL HERE
+  baseURL: `http://15.206.144.233:5000/api`,//YOUR_API_URL HERE
   // baseURL: `http://localhost:4002/api`, //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
